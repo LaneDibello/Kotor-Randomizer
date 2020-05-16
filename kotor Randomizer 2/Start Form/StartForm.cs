b9a6c5341449a310550cd90f814546b4d5462551
laned <lane.dibello@gmail.com>
2020-05-16 15:20:27 -0500

    Minor Aesthetic Adjustments added some project notes

--- kotor Randomizer 2/Start Form/StartForm.cs
+++ kotor Randomizer 2/Start Form/StartForm.cs
@@ -37,14 +37,14 @@ namespace kotor_Randomizer_2
         {
             ModuleForm MF = new ModuleForm();
             MF.Show();
-            if (!Properties.Settings.Default.module_rando_active) { module_radio_Click(module_button, new EventArgs()); }
+            if (!Properties.Settings.Default.module_rando_active) { module_radio_Click(sender, e); module_radio_MouseLeave(sender, e); }
         }
 
         private void sound_button_Click(object sender, EventArgs e)
         {
             SoundForm SF = new SoundForm();
             SF.Show();
-            if (!Properties.Settings.Default.sound_rando_active) { sound_radio_Click(sound_button, new EventArgs()); }
+            if (!Properties.Settings.Default.sound_rando_active) { sound_radio_Click(sender, e); sound_radio_MouseLeave(sender, e); }
         }
 
         private void path_button_Click(object sender, EventArgs e)
@@ -58,14 +58,14 @@ namespace kotor_Randomizer_2
         {
             ModelForm MF = new ModelForm();
             MF.Show();
-            if (!Properties.Settings.Default.model_rando_active) { model_radio_Click(model_button, new EventArgs()); }
+            if (!Properties.Settings.Default.model_rando_active) { model_radio_Click(sender, e); model_radio_MouseLeave(sender, e); }
         }
 
         private void item_button_Click(object sender, EventArgs e)
         {
             ItemForm IF = new ItemForm();
             IF.Show();
-            if (!Properties.Settings.Default.item_rando_active) { item_radio_Click(item_button, new EventArgs()); }
+            if (!Properties.Settings.Default.item_rando_active) { item_radio_Click(sender, e); item_radio_MouseLeave(sender, e); }
         }
 
         private void seed_button_Click(object sender, EventArgs e)
@@ -78,28 +78,28 @@ namespace kotor_Randomizer_2
         {
             TextureForm TF = new TextureForm();
             TF.Show();
-            if (!Properties.Settings.Default.texture_rando_active) { texture_radio_Click(texture_button, new EventArgs()); }
+            if (!Properties.Settings.Default.texture_rando_active) { texture_radio_Click(sender, e); texture_radio_MouseLeave(sender, e); }
         }
 
         private void twoda_button_Click(object sender, EventArgs e)
         {
             TwodaForm TF = new TwodaForm();
             TF.Show();
-            if (!Properties.Settings.Default.twoda_rando_active) { twoda_radio_Click(twoda_button, new EventArgs()); }
+            if (!Properties.Settings.Default.twoda_rando_active) { twoda_radio_Click(sender, e); twoda_radio_MouseLeave(sender, e); }
         }
 
         private void text_button_Click(object sender, EventArgs e)
         {
             TextF TF = new TextF();
             TF.Show();
-            if (!Properties.Settings.Default.text_rando_active) { text_radio_Click(text_button, new EventArgs()); }
+            if (!Properties.Settings.Default.text_rando_active) { text_radio_Click(sender, e); text_radio_MouseLeave(sender, e); }
         }
 
         private void other_button_Click(object sender, EventArgs e)
         {
             OtherForm OF = new OtherForm();
             OF.Show();
-            if (!Properties.Settings.Default.other_rando_active) { other_radio_Click(other_button, new EventArgs()); }
+            if (!Properties.Settings.Default.other_rando_active) { other_radio_Click(sender, e); other_radio_MouseLeave(sender, e); }
         }
     }
 }
